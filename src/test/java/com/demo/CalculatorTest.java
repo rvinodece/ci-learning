fb7e0501100824b57baf8954bd3ff25b02674091
Vinod R <rvinodece@gmail.com>
2026-06-03 13:23:49 +0530

    adde division feature

--- src/test/java/com/demo/CalculatorTest.java
+++ src/test/java/com/demo/CalculatorTest.java
@@ -13,6 +13,11 @@ public class CalculatorTest {
 
         assertEquals(4, calculator.add(2, 2));
         assertEquals(2, calculator.subtract(5,3));
+         }
+    @Test
+    void testDivide() {
+        Calculator calculator = new Calculator();
 
+        assertEquals(5, calculator.divide(10, 2));
     }
 }
